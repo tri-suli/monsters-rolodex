d3a115d994a7d765f41ae60dbb80d8390fe5dd49
tri-suli <trisulipras@gmail.com>
2021-03-06 21:38:23 +0700

    REACT-03: Search Input Component
    **WHY**
    
    Add filter functionality
    
    **WHAT**
    
    - Create search input box component
    - Update some styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,36 @@ import './App.css';
 
 import { all } from './api/users'
 import User from './entities/User';
+import SearchBox from './components/search-box/SearchBox';
 import CardList from './components/card-list/CardList';
 
 type AppState = {
-  users: Array<User>
+  users: Array<User>;
+  keyword: string;
 }
 
 class App extends Component {
   state: AppState = {
-    users: []
+    users: [],
+    keyword: ''
   };
 
   async componentDidMount () {
     this.setState({ users: await all() });
   }
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ keyword: event.target.value });
+  }
   
   render (): JSX.Element {
+    const { users, keyword } = this.state;
+
     return (
       <div className="App">
-        <CardList monsters={this.state.users} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox hint={"Search monsters..."} handleChange={this.handleChange} />
+        <CardList monsters={User.filter(keyword, users)} />
       </div>
     );
   }
